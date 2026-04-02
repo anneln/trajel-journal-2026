8382818c6b367802d6ae8e7ff274c3d634d905cd
Anne-Hélène <ahbricet@gmail.com>
2026-04-02 13:51:34 +0200

    delete app.css

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,29 +1,32 @@
 import marker from "../src/assets/marker.png";
+import japan from "../src/assets/japan.png";
 
 export default function Entry() {
   return (
     <section>
-      <img src="" className="img-url" />
-      <div className="travel-infos">
-        <div className="location-infos">
-          <img src={marker} className="marker" />
-          <p className="location">JAPAN</p>
-          <a
-            className="location-link"
-            href="https://maps.app.goo.gl/YxzD76dWkM2kPvLo6"
-            target="_blank"
-            rel="noopener"
-          >
-            View on Google Maps
-          </a>
+      <div className="container">
+        <img src={japan} className="img-url" />
+        <div className="travel-infos">
+          <div className="location-infos">
+            <img src={marker} className="marker" />
+            <p className="location">JAPAN</p>
+            <a
+              className="location-link"
+              href="https://maps.app.goo.gl/YxzD76dWkM2kPvLo6"
+              target="_blank"
+              rel="noopener"
+            >
+              View on Google Maps
+            </a>
+          </div>
+          <h3 className="travel-title">Mount Fuji</h3>
+          <p className="date">12 Jan, 2023 - 24 Jan, 2023</p>
+          <p className="description">
+            Mount Fuji is the tallest mountain in Japan, standing at 3,776
+            meters (12,380 feet). Mount Fuji is the single most popular tourist
+            site in Japan, for both Japanese and foreign tourists.
+          </p>
         </div>
-        <h3 className="travel-title">Mount Fuji</h3>
-        <p className="date">12 Jan, 2023 - 24 Jan, 2023</p>
-        <p className="description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
       </div>
     </section>
   );
