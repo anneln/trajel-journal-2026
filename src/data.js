49bef210068aaecea096d4f6718d42286bfa7e59
Anne-Hélène <ahbricet@gmail.com>
2026-04-08 11:21:43 +0200

    Add data.js files

---
+++ src/data.js
@@ -0,0 +1,55 @@
+import japan from "./src/assets/japan.png";
+import sydney from "./src/assets/Sydney.png";
+import norway from "./src/assets/Norway.png";
+import brussels from "./src/assets/Brussels.png";
+
+export default [
+  {
+    id: 1,
+    img: {
+      src: { brussels },
+      alt: "Town hall Brussels",
+    },
+    title: "Brussels",
+    country: "Belgium",
+    googleMapsLink: "https://maps.app.goo.gl/9HnfzYcTAXz4YCFf8",
+    dates: "21 apr, 2026 - 22 Apr, 2026",
+    text: "Grand Place is the central square of the City of Brussels. World-renowned for its ornate and aesthetic richness, it is bordered by guild houses, the Town Hall, and the King’s House.",
+  },
+  {
+    id: 2,
+    img: {
+      src: { japan },
+      alt: "Mount Fuji",
+    },
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+    dates: "20 Oct, 2024 - 1 Nov, 2024",
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 3,
+    img: {
+      src: { sydney },
+      alt: "Sydney Opera House",
+    },
+    title: "Sydney Opera House",
+    country: "Australia",
+    googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+    dates: "27 May, 2022 - 8 Jun, 2022",
+    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+  },
+  {
+    id: 4,
+    img: {
+      src: { norway },
+      alt: "Geirangerfjord",
+    },
+    title: "Geirangerfjord",
+    country: "Norway",
+    googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+    dates: "01 Oct, 2019 - 18 Oct, 2019",
+    text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  },
+];
