ed80b9624deafe62de99e3abae90a0e2c1e0f88b
Anne-Hélène <ahbricet@gmail.com>
2026-04-09 10:47:53 +0200

    add styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,8 @@ import globe from "../src/assets/globe.png";
 export default function Header() {
   return (
     <header>
-      <div>
-        <img className="logo" src={globe} alt="globe-icon" />
-        <h1>my travel journal</h1>
-      </div>
+      <img className="logo" src={globe} alt="globe-icon" />
+      <h1>my travel journal</h1>
     </header>
   );
 }
